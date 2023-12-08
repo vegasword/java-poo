9c4beae230792bf7f191f981016af070bca0de1a
Tom Bourti <sirvos.lol@gmail.com>
2023-12-08 13:56:22 +0100

    commentaire by tom

--- src/view/SortieInterfaceUtilisateur.java
+++ src/view/SortieInterfaceUtilisateur.java
@@ -1,35 +1,67 @@
 package view;
 
 import java.util.ArrayList;
+import models.*; // Import des modèles nécessaires, comme Joueur, Ennemi, Personnage, Objet, etc.
 
-import models.*;
-
+/**
+ * Classe pour l'interface utilisateur de sortie, affichant différents éléments du jeu.
+ */
 public class SortieInterfaceUtilisateur {
+
+   /**
+    * Affiche le menu principal du jeu.
+    */
    public void menuPrincipal()  {
       System.out.println("-~-~- FAIR FIGHT -~-~-\n1. Play\n2. Quit");
    }
 
+   /**
+    * Affiche les détails du joueur et des ennemis.
+    * @param joueur L'objet Joueur représentant le joueur.
+    * @param ennemis Liste des ennemis actuellement dans le jeu.
+    */
    public void afficherJeu(Joueur joueur, ArrayList<Ennemi> ennemis) {
+      // Affichage des informations du joueur
       System.out.println(joueur.getNom() + " : " + joueur.getPv() + " PV, " + joueur.getForce() + " FORCE, " + joueur.getOr() + " OR");
+
+      // Boucle pour afficher les informations de chaque ennemi
       for (Ennemi ennemi : ennemis) {
          System.out.println(ennemi.getNom() + " : " + ennemi.getPv() + " PV, " + ennemi.getForce() + " FORCE");
       }
+
+      // Options disponibles pour le joueur
       System.out.println("-----------------------\n1. Attaque\n2. Défendre\n3. Utiliser objet");
    }
 
+   /**
+    * Affiche une interaction entre un personnage et un objet.
+    * @param personnage Le personnage impliqué dans l'interaction.
+    * @param objet L'objet utilisé ou ramassé.
+    * @param utilisation Booléen indiquant si l'objet est utilisé (true) ou ramassé (false).
+    */
    public void interactionPersonnageObject(Personnage personnage, Objet objet, boolean utilisation) {
-     if (utilisation) {
-        System.out.println(personnage.getNom() + " utilise " + objet.getNom());
-     } else {
-        System.out.println(personnage.getNom() + " ramasse " + objet.getNom());
-     }
+      if (utilisation) {
+         System.out.println(personnage.getNom() + " utilise " + objet.getNom());
+      } else {
+         System.out.println(personnage.getNom() + " ramasse " + objet.getNom());
+      }
    }
 
+   /**
+    * Affiche les détails d'une attaque entre deux personnages.
+    * @param a Le personnage attaquant.
+    * @param b Le personnage défendant.
+    * @param degat Les dégâts infligés lors de l'attaque.
+    */
    public void afficherAttaque(Personnage a, Personnage b, int degat) {
       System.out.println(a.getNom() + " attaque " + b.getNom() + " en lui infligeant " + degat + " dégâts !");
    }
 
-   // ToDo : afficher boutique
+   /**
+    * Méthode pour afficher les options disponibles pendant l'entracte.
+    * ToDo : Implémenter l'affichage de la boutique.
+    * @param joueur L'objet Joueur représentant le joueur.
+    */
    public void afficherEntracte(Joueur joueur)
    {
       System.out.println("-----------------------\n" +
