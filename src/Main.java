89e0fd626e63227d60897a9cc2816660ac8bdc75
Alexandre Perché <a.perche.dev@gmail.com>
2023-12-08 12:00:00 +0100

    Equilibrage du jeu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import controllers.*;
 
 public class Main {
     public static void main(String[] args) {
-        Jeu jeu = new Jeu(3);
+        Jeu jeu = new Jeu();
         if (jeu.initialiseJeu()) {
             jeu.boucleDeJeu();
         }
